1fac01dfc05c667873630d5b71604152de299a1b
yanbaru1331 <s24012@g.metro-cit.ac.jp>
2024-05-08 19:36:37 +0900

    環境整備とtest

--- back/src/index.ts
+++ back/src/index.ts
@@ -7,6 +7,10 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+// app.get post putをおいて('urlパス, (c) => {処理の内容})
+app.get('/hoge/huga', (c) => {
+  return c.text('poyp~')
+})
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
