47a2f16f62c7843622e690e35078c4e102442a2a
yanbaru1331 <s24012@g.metro-cit.ac.jp>
2024-05-27 14:24:49 +0900

    pullされたもの修正 pnpm iはコンテナ中で一応動かす

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,95 +1,6 @@
-// import { serve } from "@hono/node-server";
-// import { Hono } from "hono";
-// import { validator } from "hono/validator";
-// import { cors } from 'hono/cors'
-
-// const app = new Hono();
-
-
-// //honoでcrosをlocalhost:3001に許可するようなコード
-// app.use(cors({
-// 	origin: ['http://localhost:3001'],
-// 	allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'], // ここに追加
-// 	allowMethods: ['POST', 'GET', 'OPTIONS'],
-// 	exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
-// 	maxAge: 600,
-// 	credentials: true,
-// }))
-
-
-
-// app.get("/", (c) => {
-//   return c.text("Hello Hono!");
-// });
-
-// // app.get post putをおいて('urlパス, (c) => {処理の内容})
-// app.get("/hoge/huga", async (c) => {
-//   return c.text("poyp~");
-// });
-// const port = 3000;
-// console.log(`Server is running on port ${port}`);
-
-// app.post(
-//   "/login",
-//   validator("json", (value, c) => {
-//     //受け付けた引数が正しい型、存在するかを確認するコード
-//     const user = value["user"];
-//     const pass = value["password"];
-//     if (
-//       !user ||
-//       typeof user !== "string" ||
-//       !pass ||
-//       typeof pass !== "string"
-//     ) {
-//       return c.text("Invalid!", 400);
-//     }
-//     return { user, pass };
-//   }),
-//   async function (c) {
-//     const { user, pass } = c.req.valid("json");
-//     //最終的にdbとくっつける
-//     if (user === "admin" && pass === "admin") {
-//       return c.json({ result: "ok" });
-//     } else {
-//       return c.json({ result: "passNg" }, 400);
-//     }
-//   }
-// );
-
-// app.get("/test-cors", (c) => {
-// 	return c.json({ message: "CORS is working!" });
-//   });
-
-// // //honoでcrosをlocalhost:3001に許可するようなコード
-// // app.use(cors({
-// // 	origin: ['http://localhost:3001'],
-// // 	allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'], // ここに追加
-// // 	allowMethods: ['POST', 'GET', 'OPTIONS'],
-// // 	exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
-// // 	maxAge: 600,
-// // 	credentials: true,
-// // }))
-
-
-
-
-
-// // app.use('/*', cors()
-// // 	// cors({
-// // 	// 	origin: ['http://localhost:3001/*'],
-// // 	// 	allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'], // ここに追加
-// // 	// 	allowMethods: ['POST', 'GET', 'OPTIONS'],
-// // 	// 	exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
-// // 	// 	maxAge: 600,
-// // 	// 	credentials: true,
-// // 	// })
-// // )
-// serve({
-//   fetch: app.fetch,
-//   port,
-// });
 
 // // async->非同期の通信 dbとかを触れる可能性があるならいれる
+// 型定義はちゃん書いておく
 
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
@@ -100,9 +11,9 @@ const app = new Hono();
 
 // CORSミドルウェアの設定
 app.use('/*', cors({
-  // origin: "http://frontend:3001",
+  origin: "http://front:3001",
   //オリジンの設定がうまく言ってないのでとりあえず*で動かす
-  origin: "*",
+  // origin: "*",
   //   allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
   allowHeaders: ['*'],
   allowMethods: ['POST', 'GET', 'OPTIONS'],
@@ -113,6 +24,8 @@ app.use('/*', cors({
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+
+  //db操作するコード
 });
 
 app.get("/hoge/huga", async (c) => {
