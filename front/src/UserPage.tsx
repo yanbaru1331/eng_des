6e08683f1f1f1c28800c352baf15644cb28fb7aa
yanbaru1331 <s24012@g.metro-cit.ac.jp>
2024-09-26 16:56:39 +0900

    del対応できるようにした

--- front/src/UserPage.tsx
+++ front/src/UserPage.tsx
@@ -6,7 +6,7 @@ import Modal from '../components/Modal';
 import axios from "axios";
 
 
-const UserPage: React.FC = ()=> {
+const UserPage: React.FC = () => {
     const { userid } = useParams();
     //modalの表示非表示を管理するstate
     const [showModal, setShowModal] = useState(false);
@@ -17,13 +17,14 @@ const UserPage: React.FC = ()=> {
     };
 
     const ShowModal = () => {
-        axios.get("http://localhost:3000/api/portfolio/page?user_id="+sessionStorage.getItem('userId'))
-        .then(() => {
-            navigate(`/userpage/${sessionStorage.getItem('userId')}/chart`);
-        })
-        .catch(() => {
-        setShowModal(true);
-        });
+        axios.get("http://localhost:3000/api/portfolio/page?user_id=" + sessionStorage.getItem('userId'))
+            .then(() => {
+                console.log("success");
+                navigate(`/userpage/${sessionStorage.getItem('userId')}/chart`);
+            })
+            .catch(() => {
+                setShowModal(true);
+            });
     };
 
     const viewChart = () => {
@@ -31,33 +32,37 @@ const UserPage: React.FC = ()=> {
     }
 
     const delChart = () => {
-        axios.post("http://localhost:3000/api/portfolio/page", {user_id: sessionStorage.getItem('userId')}, 
-        {
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
+        axios.delete("http://localhost:3000/api/portfolio/page",
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: { user_id: Number(sessionStorage.getItem('userId')) }
+            })
+            .then((res) => {
+                console.log(res);
+            })
+    }
 
-        }
 
     return (
-        <div> 
-        <h1>Hello {userid}!</h1>
-        <Button onClick={viewChart}>チャートを標示</Button>
-        {
-            //ここでログインしているユーザーが自分自身のページの時だけ管理モードを表示
-            //バックとネゴシエーションできたら個々の処理を書き直しておく
-            sessionStorage.getItem('AUTHORITY') !== "" &&
-            <div>
-            <Button type="submit" onClick={ShowModal}>チャート登録・編集</Button>
-            <Button type="submit" onClick={delChart}>チャート削除</Button>
-            <p></p>
-            <Button onClick={Logout}>ログアウト</Button>
-            <Modal showFlag={showModal} setShowFlag={setShowModal} type="edit"/>
-            </div>
-        }
+        <div>
+            <h1>Hello {userid}!</h1>
+            <Button onClick={viewChart}>チャートを標示</Button>
+            {
+                //ここでログインしているユーザーが自分自身のページの時だけ管理モードを表示
+                //バックとネゴシエーションできたら個々の処理を書き直しておく
+                sessionStorage.getItem('AUTHORITY') !== "" &&
+                <div>
+                    <Button type="submit" onClick={ShowModal}>チャート登録・編集</Button>
+                    <Button type="submit" onClick={delChart}>チャート削除</Button>
+
+                    <Button onClick={Logout}>ログアウト</Button>
+                    <Modal showFlag={showModal} setShowFlag={setShowModal} type="edit" />
+                </div>
+            }
         </div>
-        
+
     );
 }
 
